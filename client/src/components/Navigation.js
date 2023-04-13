9495049e7268eaa8ffc5bf109f36d6e9253e6b19
joe <jajohnson0201@gmail.com>
2023-04-12 21:39:46 -0500

    added components

---
+++ client/src/components/Navigation.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+
+
+export default function Nav({ currentPage, handlePageChange }) {
+    const [loggedIn, changeLoggin] = useState("false");
+    if (currentPage = 'AddWorkout') {
+        changeLoggin("true");
+    }
+    if (loggedIn === "true") {
+        return (
+            <ButtonGroup>
+                <Button>
+                    <a
+                        href='#AddWorkout'
+                        onClick={() => handlePageChange("AddWorkout")}
+                        className={currentPage === 'AddWorkout' ? 'nav-link active' : 'nav-link'}>
+                        Add Workout
+                    </a>
+                </Button>
+            </ButtonGroup>
+        )
+    } else {
+        return (
+            <div>
+                <ButtonGroup variant="contained" aria-label="outlined primary button group">
+
+                    <Button>
+                        <a
+                            href='#Login'
+                            onClick={() => handlePageChange("Login")}
+                            className={currentPage === 'Login' ? 'nav-link active' : 'nav-link'}>
+                            Login
+                        </a>
+                    </Button>
+                    <Button>
+                        <a
+                            href='#SignUp'
+                            onClick={() => handlePageChange("SignUp")}
+                            className={currentPage === 'SignUp' ? 'nav-link active' : 'nav-link'}>
+                            Sign Up
+                        </a>
+                    </Button>
+                    <Button>
+                        <a
+                            href='#AddWorkout'
+                            onClick={() => handlePageChange("AddWorkout")}
+                            className={currentPage === 'AddWorkout' ? 'nav-link active' : 'nav-link'}>
+                            Add Workout
+                        </a>
+                    </Button>
+                </ButtonGroup>
+            </div>
+        );
+    }
+}
+
+
+
--- client/src/pages/Dash.js
+++ client/src/pages/Dash.js
@@ -1,16 +1,40 @@
 import React, { useState } from 'react';
 import { render } from "react-dom";
 import Calendar from 'react-calendar';
+import Nav from '../components/Navigation';
+import Login from './SignIn';
+import SignUp from './SignUp';
+import AddWorkout from '../components/AddWorkout';
+
 
 const ReactCalendar = () => {
     const [date, setDate] = useState(new Date())
 
     const onChange = date => {
-     setDate(date)   
+        setDate(date)
     }
-    return <div>
-        <Calendar onChange={onChange} value={date}/>
+
+    const [currentPage, setCurrentPage] = useState('Login');
+    const renderPage = () => {
+        if (currentPage === 'Login') {
+            return <Login />;
+        }
+        if (currentPage === 'SignUp') {
+            return <SignUp />;
+        }
+        if (currentPage === 'AddWorkout') {
+            return <AddWorkout />;
+        }
+        return <Resume />;
+    };
+    const handlePageChange = (page) => setCurrentPage(page);
+
+    return (<div>
+        <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
+        <AddWorkout />
+        <Calendar onChange={onChange} value={date} />
     </div>
+    )
 };
 
-render (<ReactCalendar/>, document.querySelector("#root"));
+render(<ReactCalendar />, document.querySelector("#root"));
