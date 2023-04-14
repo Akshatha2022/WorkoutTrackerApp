f997c46c5b59f4ed3dfd2da5955f7ae46db9b37b
Akshatha2022 <115680614+Akshatha2022@users.noreply.github.com>
2023-04-13 20:47:12 -0500

    modify recentactivity

--- client/src/components/RecentActivity.js
+++ client/src/components/RecentActivity.js
@@ -4,11 +4,10 @@ import { useQuery, useMutation } from '@apollo/client';
 import { QUERY_USER } from '../utils/queries';
 
 const RecentActivity = () => {
-
+    const { loading, error, data } = useQuery(QUERY_USER);
     const getUserWorkouts = async (event) => {
         event.preventDefault();
-        const { loading, error, data } = useQuery(QUERY_USER);
-       const workouts = data?.user.workouts || [];
+        const workouts = data?.user.workouts || [];
         return workouts;
     }
     const workouts = getUserWorkouts();
